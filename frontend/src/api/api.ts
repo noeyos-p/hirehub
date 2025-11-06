a5341dbdbd3376fd49bef4b3d6de8ec7e20c3300
MINSsss96 <minsue0422@naver.com>
2025-11-06 18:27:49 +0900

    강현님 깜짝 선물#2

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://byeongarigaebaldan.store",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://byeongarigaebaldan.store",
   withCredentials: true,
 });
 
